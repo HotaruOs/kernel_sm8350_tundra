381a509fe676a19a544ff1c84793bd48df90fa97
Hotaro Pão <149913511+HotaruOs@users.noreply.github.com>
2024-11-08 13:50:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 kernel_dir="${PWD}"
 CCACHE=$(command -v ccache)
 objdir="${kernel_dir}/out"
-anykernel=$HOME/anykernel
+anykernel=${PWD}/anykernel
 builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
-kernel_name="torracat-tundra"
+kernel_name="HuP"
 zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
 TC_DIR="${PWD}/tc"
 CLANG_DIR=$TC_DIR/clang-r522817
 export CONFIG_FILE="vendor/tundra-qgki_defconfig"
 export ARCH="arm64"
-export KBUILD_BUILD_HOST=raghavt20
-export KBUILD_BUILD_USER=raghav
+export KBUILD_BUILD_HOST=HuP
+export KBUILD_BUILD_USER=HotaruOs
 
 export PATH="$CLANG_DIR/bin:$PATH"
 if ! [ -d "$TC_DIR" ]; then
@@ -58,7 +58,7 @@ completion() {
     COMPILED_DTB=arch/arm64/boot/dts/vendor/qcom/lahaina*dtb
     if [[ -f ${COMPILED_IMAGE} ]]; then
 
-        git clone -q https://github.com/raghavt20/AnyKernel3 -b tundra $anykernel
+        git clone -q https://github.com/HotaruOs/AnyKernel3 -b HuP $anykernel
 
         cp -f ${COMPILED_IMAGE} $anykernel
         cp -f "${DTB_DIR}"/*.img $anykernel
@@ -69,11 +69,14 @@ completion() {
         find . -name "*.zip" -type f -delete
         zip -r AnyKernel.zip *
         cp AnyKernel.zip $zip_name
-        cp $anykernel/$zip_name $HOME/$zip_name
+        cp $anykernel/$zip_name ${PWD}/$zip_name
         rm -rf $anykernel
         END=$(date +"%s")
         DIFF=$(($END - $START))
-        curl -F "file=@$HOME/$zip_name" https://temp.sh/upload
+        DIFF=$(($END - $START))
+        echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+        UPLOAD_URL=$(curl -F "file=@${PWD}/$zip_name" "http://bashupload.com/$(basename "$zip_name")")
+        echo "Link de download: $UPLOAD_URL"
         echo -e ${LGR} "############################################"
         echo -e ${LGR} "############# OkThisIsEpic!  ##############"
         echo -e ${LGR} "############################################${NC}"
