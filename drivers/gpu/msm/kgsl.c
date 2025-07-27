a16155d24ea97b50be9e4ccc3fa1f3d503761167
HotaruOs <arroztlg@gmail.com>
2025-07-26 22:05:43 -0300

    Revert "msm: kgsl: Use kthread instead of workqueue for event work"
    This reverts commit 19075a3e43030e147af0633668236fed34aebf86.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -672,11 +672,11 @@ int kgsl_context_init(struct kgsl_device_private *dev_priv,
 	if (id == -ENOSPC) {
 		/*
 		 * Before declaring that there are no contexts left try
-		 * flushing the event worker just in case there are
+		 * flushing the event workqueue just in case there are
 		 * detached contexts waiting to finish
 		 */
 
-		kthread_flush_worker(device->events_worker);
+		flush_workqueue(device->events_wq);
 		id = _kgsl_get_context_id(device);
 	}
 
@@ -4511,15 +4511,14 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	device->pwrctrl.interrupt_num = status;
 	disable_irq(device->pwrctrl.interrupt_num);
 
-	device->events_worker = kthread_create_worker(0, "kgsl-events");
+	device->events_wq = alloc_workqueue("kgsl-events",
+		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS | WQ_HIGHPRI, 0);
 
-	if (IS_ERR(device->events_worker)) {
-		status = PTR_ERR(device->events_worker);
-		dev_err(device->dev, "Failed to create events worker ret=%d\n", status);
+	if (!device->events_wq) {
+		dev_err(device->dev, "Failed to allocate events workqueue\n");
+		status = -ENOMEM;
 		goto error_pwrctrl_close;
 	}
-
-	sched_set_fifo(device->events_worker->task);
 
 	/* This can return -EPROBE_DEFER */
 	status = kgsl_mmu_probe(device);
@@ -4545,8 +4544,10 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	return 0;
 
 error_pwrctrl_close:
-	if (!IS_ERR(device->events_worker))
-		kthread_destroy_worker(device->events_worker);
+	if (device->events_wq) {
+		destroy_workqueue(device->events_wq);
+		device->events_wq = NULL;
+	}
 
 	kgsl_pwrctrl_close(device);
 error:
@@ -4556,7 +4557,10 @@ error:
 
 void kgsl_device_platform_remove(struct kgsl_device *device)
 {
-	kthread_destroy_worker(device->events_worker);
+	if (device->events_wq) {
+		destroy_workqueue(device->events_wq);
+		device->events_wq = NULL;
+	}
 
 	kgsl_device_snapshot_close(device);
 
